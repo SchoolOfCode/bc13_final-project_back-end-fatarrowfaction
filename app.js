49eabee948bb1ea1effd11b062bb1c67db9aa68e
MikeChara <mikejchara@gmail.com>
2023-01-12 16:07:01 +0000

    added backend user verify/auth/requests

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 import express from "express";
+import cors from "cors";
 
 // const reviewsRouter = require("./routes/reviews.js");
 import { userFoodRouter } from "./Routes/foodlist.js";
+import { userRouter } from "./Routes/user.js";
 
 const app = express();
 const PORT = process.env.port || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/pantry", userFoodRouter);
+app.use("/userVerify", userRouter);
 
 app.listen(PORT, function () {
-	console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
