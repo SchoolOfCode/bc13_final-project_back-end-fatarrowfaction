df861c3154ae44d631148d63219afea4ead89aa2
Mohamed Abdi <mohamed.abdi11@hotmail.co.uk>
2023-01-17 12:04:02 +0000

    patchBinned added to imports

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
 import {
-	getUserFood,
-	getStorageID,
-	postFood,
-	getUserProfile,
-  patchEatenDate
+  getUserFood,
+  getStorageID,
+  postFood,
+  getUserProfile,
+  patchEatenDate,
+  patchBinnedDate,
 } from "../Models/models.js";
 //gets from Pantry screen
 export const userFoodRouter = express.Router();
@@ -24,10 +25,15 @@ userFoodRouter.get("/:id", async function (req, res) {
   res.json({ success: true, payload: foodArray });
 });
 
-userFoodRouter.patch("/id", async function (req, res){
-	const editedFood = await patchFoodDate(req.body.eaten_on, req.body.binned_on, req.body.donated_on, req.params.id);
-	res.json({success: true, payload: editedFood})
-})
+userFoodRouter.patch("/id", async function (req, res) {
+  const editedFood = await patchFoodDate(
+    req.body.eaten_on,
+    req.body.binned_on,
+    req.body.donated_on,
+    req.params.id
+  );
+  res.json({ success: true, payload: editedFood });
+});
 
 // whenever a user posts a new item, we need to first send a get request to get the container ID and then a post request to put the new food item in there to the container ID
 newItemRouter.get("/:id", async function (req, res) {
