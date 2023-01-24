8306bcdb44f8f9369689e69679681a8d81bb95f3
PervezZz <63874062+nicholaspervez@users.noreply.github.com>
2023-01-24 23:18:51 +0000

    changed some stats stuff for front end

--- HelperFunctions/calculatePercentages.js
+++ HelperFunctions/calculatePercentages.js
@@ -1,20 +1,29 @@
 export function eatenStats(dataArray) {
-  const numberOfEaten = dataArray.filter((e) => e.eaten_on).length;
-  const arrayOfWasted = dataArray.filter((e) => e.binned_on);
-  const eatenPercentage =
-      (numberOfEaten / (arrayOfWasted.length + numberOfEaten)) * 100;
-  const wastedPercentage =
-      (arrayOfWasted.length / (numberOfEaten + arrayOfWasted.length)) * 100;
-
-  // for loop to iterate through the arrayofwasted to find how much money has been wasted
-  let wastedCost = 0;
-  for (let fooditem of arrayOfWasted) {
-      wastedCost = wastedCost + fooditem.price;
+    const arrayOfEaten = dataArray.filter((e) => e.eaten_on);
+    const arrayOfWasted = dataArray.filter((e) => e.binned_on);
+    const eatenPercentage =
+        (arrayOfEaten.length / (arrayOfWasted.length + arrayOfEaten.length)) * 100;
+    const wastedPercentage =
+        (arrayOfWasted.length / (arrayOfEaten.length + arrayOfWasted.length)) * 100;
+  
+    // for loop to iterate through the arrayofwasted to find how much money has been wasted
+    let wastedCost = 0;
+    for (let fooditem of arrayOfWasted) {
+        wastedCost = wastedCost + fooditem.price;
+    }
+  
+    let spentWell = 0;
+    for (let fooditem of arrayOfEaten){
+      spentWell = spentWell + fooditem.price
+    }
+  
+    return {
+        eatenPercentage: Math.round(eatenPercentage),
+        wastedPercentage: Math.round(wastedPercentage),
+        moneyWasted: wastedCost,
+        spentWell: spentWell,
+        totalNumberEaten: arrayOfEaten.length,
+        totalNumberWasted: arrayOfWasted.lengnth,
+        totalNumberItems: arrayOfEaten.lengnth + arrayOfWasted.lengnth,
+    };
   }
-
-  return {
-      eatenPercentage: Math.round(eatenPercentage),
-      wastedPercentage: Math.round(wastedPercentage),
-      moneyWasted: wastedCost,
-  };
-}
