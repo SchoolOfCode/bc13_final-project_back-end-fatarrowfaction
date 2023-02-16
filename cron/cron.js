a324596953d0c86eccacf6e3da8abfc7a27daef6
PervezZz <63874062+nicholaspervez@users.noreply.github.com>
2023-02-16 16:52:41 +0000

    working and set to 9am

--- cron/cron.js
+++ cron/cron.js
@@ -1,29 +1,39 @@
-import { schedule } from 'node-cron';
-import axios from 'axios';
-import { getTodaysFood } from '../Models/models.js';
+import { schedule } from "node-cron";
+import axios from "axios";
+import { getTodaysFood } from "../Models/models.js";
+import { getAllUsers } from "../Models/models.js";
 
-console.log('cron.js is live')
-
-async function getNotification(){
-    try{
-    console.log('fired');
-    const uid = '303Ut9TLrAQjyq5hlJrmlsB66Tl2';
-    const todaysFood = await getTodaysFood(uid);
-      const data =  JSON.stringify(todaysFood);
-     axios.post(`https://app.nativenotify.com/api/indie/notification`, {
+async function forEachFunction(uid){
+try {
+    console.log('trying this')
+  const todaysFood = await getTodaysFood(uid);
+  console.log('todays food :', todaysFood)
+//   const data = JSON.stringify(todaysFood);
+//   console.log('data : ', data)
+  if (todaysFood.length > 0) {
+    axios.post(`https://app.nativenotify.com/api/indie/notification`, {
       subID: `${uid}`,
       appId: 6107,
-      appToken: 'RscfdJGUHSLru74JJd6STe',
-      title: 'Food due to expire today...',
-      message: `You have ${data.length} items going off today, better eat em up!`
- });}
- catch(exception){
-    console.log(exception)
- }
-    }
-
-    schedule('* * * * * ', async() => getNotification(), {
-      scheduled: true,
-      timezone: "Europe/London"
+      appToken: "RscfdJGUHSLru74JJd6STe",
+      title: "Food due to expire today...",
+      message: `You have ${todaysFood.length} items going off today, better eat em up!`,
     });
+  }
+} catch (exception) {
+  console.log(exception);
+}
+}
+async function getNotification() {
+    console.log('cronnybobs')
+    const userArray = await getAllUsers()
+    userArray.forEach((obj)=>{
+        console.log(obj.uid)
+        forEachFunction(obj.uid)
+    })
+}
 
+
+schedule("0 9 * * * ", async () => getNotification(), {
+  scheduled: true,
+  timezone: "Europe/London",
+});
